d87e7342dc70137aafb1c7984ea053fc80293420
limingyang <limingyang0314@qq.com>
2019-03-11 12:31:07 +0800

    ok

--- model/pictures.php
+++ model/pictures.php
@@ -1,11 +1,21 @@
 <?php
 include 'mysql.php';
+
+/*
+**处理查询结果
+*/
+function finish_picture_select($type, $result){
+    $result = getDataAsArray($result);
+    return $result;
+}
+
+
 /*
 **上传成功的图片插入数据库
 */
 function upload_into_db($type,$file_name,$conn){
     //include 'mysql.php';
-    $sql = "INSERT INTO {$type} (b_name) VALUES ({$file_name})";
+    $sql = "INSERT INTO {$type} (b_name) VALUES ('{$file_name}')";
     $conn->query($sql);
 }
 
@@ -30,38 +40,42 @@ function upload_picture($type,$conn){
        || ($_FILES["file"]["type"] == "image/png"))
        && ($_FILES["file"]["size"] < 204800)   // 小于 200 kb
        && in_array($extension, $allowedExts))
-    {
+        {
         if ($_FILES["file"]["error"] > 0)
         {
             echo "错误：: " . $_FILES["file"]["error"] . "<br>";
-        }else{
-            echo "上传文件名: " . $_FILES["file"]["name"] . "<br>";
-            echo "文件类型: " . $_FILES["file"]["type"] . "<br>";
-            echo "文件大小: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-            echo "文件临时存储的位置: " . $_FILES["file"]["tmp_name"] . "<br>";
+            }else{
+                echo "上传文件名: " . $_FILES["file"]["name"] . "<br>";
+                echo "文件类型: " . $_FILES["file"]["type"] . "<br>";
+                echo "文件大小: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+                echo "文件临时存储的位置: " . $_FILES["file"]["tmp_name"] . "<br>";
         
         // 判断当期目录下的 upload 目录是否存在该文件
         // 如果没有 upload 目录，你需要创建它，upload 目录权限为 777
         
-            if (file_exists("upload/{$type}/" . $_FILES["file"]["name"])){
-                echo $_FILES["file"]["name"] . " 文件已经存在。 ";
-            }else{
+                $_FILES["file"]["name"] = "banner_".time().".".$extension;
+                if (file_exists("upload/{$type}/" . $_FILES["file"]["name"])){
+                    echo $_FILES["file"]["name"] . " 文件已经存在。 ";
+                }else{
             // 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
-                move_uploaded_file($_FILES["file"]["tmp_name"], "../upload/{$type}/" . $_FILES["file"]["name"]);
-                echo "文件存储在: " . "upload/{$type}/" . $_FILES["file"]["name"];
-                upload_into_db($type,$_FILES["file"]["name"],$conn);
+                    move_uploaded_file($_FILES["file"]["tmp_name"], "../upload/{$type}/" . $_FILES["file"]["name"]);
+                    echo "文件存储在: " . "upload/{$type}/" . $_FILES["file"]["name"];
+                    upload_into_db($type,$_FILES["file"]["name"],$conn);
+                }
             }
+        }else{
+            echo "非法的文件格式";
         }
-    }else{
-        echo "非法的文件格式";
+
     }
 
     /*
     **获取数量为number的最近banner
     */
-    function get_banners($number){
-
-
+    function get_banners($number,$conn){
+        $sql = "SELECT * FROM banners ORDER BY time DESC";
+        $result = mysqli_query($conn,$sql);
+        return finish_picture_select('banners',$result);
     }
 
     /*
@@ -77,6 +91,3 @@ function upload_picture($type,$conn){
     function get_item_picture($pID){
 
     }
-
-
-}
