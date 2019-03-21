d9ff51d1d7d5807899e310be9b2c4baf3eabd9ec
limingyang <limingyang0314@qq.com>
2019-03-21 22:45:37 +0800

    fix get openID

--- model/users.php
+++ model/users.php
@@ -3,7 +3,7 @@ require_once "mysql.php";
 
 function get_openID($js_code){
     $url = "https://api.weixin.qq.com/sns/jscode2session?appid=wxc24a817201be0ebc&secret=17b371bd3f834c1c782afeafb7bc3575&js_code={$js_code}&grant_type=authorization_code";
-    require_once("curl.php");
+    require_once("../curl.php");
     $result = getToken($url);
     session_start();
     $_SESSION['openID'] = "omfHM4iU0EA1jCLmUh43itEhtpcc";//json_decode($result)->openid;
