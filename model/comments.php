3d2c3fe8e59fa26461cfc25031f66bf2de7c8c37
limingyang <limingyang0314@qq.com>
2019-05-17 20:26:33 +0800

    fix bug

--- model/comments.php
+++ model/comments.php
@@ -45,8 +45,8 @@ function select_comment_by_openID($openID,$limit,$page, $conn){
 function insert_comment($cType, $pointerID, $pointerID2, $openID, $content, $conn){
     $sql = "INSERT INTO comments (cType,pointerID,pointerID2,openID,content) VALUES ({$cType},{$pointerID},{$pointerID2},'{$openID}','{$content}')";
     $result = mysqli_query($conn, $sql);
-    echo $sql;
-    exit;
+    //echo $sql;
+    //exit;
     if($result){
         $table = 'articles';
         $ID_name = 'aID';
