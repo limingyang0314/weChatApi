ef5163dfb60f74c3fecccf5151c41d844b279208
limingyang <limingyang0314@qq.com>
2019-05-19 17:47:14 +0800

    fix bug

--- model/items.php
+++ model/items.php
@@ -4,7 +4,7 @@ require_once 'mysql.php';
 function select_item_by_id($id, $conn){
     //echo $id;
     $sql = "SELECT I.iID AS ID, U.username, U.avatar, I.item_info AS content, T.type_name, I.hot, I.time ,I.comment_num, I.expect_price, I.contact_way, S.school_name, S.location_id
-    FROM items I, item_types T, users U,schools S,schools S
+    FROM items I, item_types T, users U, schools S
     WHERE I.iID = $id 
     AND T.type_id = I.iType_ID 
     AND  S.sID = U.school_id
