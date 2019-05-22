734e9f3bfbb36cce80bd30b9895afb894f9f5c99
limingyang <limingyang0314@qq.com>
2019-05-22 17:05:59 +0800

    fix bug

--- test.php
+++ test.php
@@ -1,7 +1,24 @@
 <?php
-session_start();
-if(isset($_SESSION['openID'])){
-    echo $_SESSION['openID'];
-}else{
-    echo "hello,world!";
-}
+require_once("model/mysql.php");
+$sql = "DELETE FROM articles WHERE openID = 'ozInc4iTuZJm9Fx6kSc_Z3pRGdCE'";
+mysqli_query($GLOBALS['conn'], $sql);
+
+$sql = "DELETE FROM items WHERE openID = 'ozInc4iTuZJm9Fx6kSc_Z3pRGdCE'";
+mysqli_query($GLOBALS['conn'], $sql);
+
+$sql = "DELETE FROM comments WHERE openID = 'ozInc4iTuZJm9Fx6kSc_Z3pRGdCE'";
+mysqli_query($GLOBALS['conn'], $sql);
+
+$sql = "DELETE FROM messages WHERE from_who = 'ozInc4iTuZJm9Fx6kSc_Z3pRGdCE";
+mysqli_query($GLOBALS['conn'], $sql);
+
+$sql = "DELETE FROM messages WHERE to_who = 'ozInc4iTuZJm9Fx6kSc_Z3pRGdCE'";
+mysqli_query($GLOBALS['conn'], $sql);
+
+$sql = "DELETE FROM collections WHERE openID = 'ozInc4iTuZJm9Fx6kSc_Z3pRGdCE'";
+mysqli_query($GLOBALS['conn'], $sql);
+
+$sql = "DELETE FROM users WHERE openID = 'ozInc4iTuZJm9Fx6kSc_Z3pRGdCE'";
+mysqli_query($GLOBALS['conn'], $sql);
+
+echo "DELETE FINISHED!";
