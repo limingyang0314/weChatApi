1be869830ed2944120181e7ee987b79399cfc4c7
limingyang <limingyang0314@qq.com>
2019-05-25 21:56:18 +0800

    fix bug

--- model/articles.php
+++ model/articles.php
@@ -42,7 +42,7 @@ require_once 'mysql.php';
     */
     function select_article_by_id($aID, $conn){
         //echo $_SESSION['openID'];
-        $sql = "SELECT A.aid AS ID, A.content, A.comment_num, T.type_name, A.hot, U.username, U.avatar, A.time, U.openID , A.comment_num, S.school_name, S.location_id, A.latitude,A.longitude,A.addresss
+        $sql = "SELECT A.aid AS ID, A.content, A.comment_num, T.type_name, A.hot, U.username, U.avatar, A.time, U.openID , A.comment_num, S.school_name, S.location_id, A.latitude,A.longitude,A.address
         FROM articles A, users U, article_types T,schools S
         WHERE A.aID = {$aID} 
         AND T.type_id = A.type_id 
@@ -59,7 +59,7 @@ require_once 'mysql.php';
     */
     function select_article_by_author($openID, $limit, $page, $conn){
         $start = $limit * ($page - 1);
-        $sql = "SELECT A.aid AS ID, A.content, A.comment_num, T.type_name, A.hot, U.username, U.avatar, A.time, U.openID, A.comment_num, S.school_name, S.location_id, A.latitude,A.longitude,A.addresss
+        $sql = "SELECT A.aid AS ID, A.content, A.comment_num, T.type_name, A.hot, U.username, U.avatar, A.time, U.openID, A.comment_num, S.school_name, S.location_id, A.latitude,A.longitude,A.address
         FROM articles A, users U, article_types T ,schools S
         WHERE A.openID = '$openID' 
         AND T.type_id = A.type_id 
@@ -110,7 +110,7 @@ require_once 'mysql.php';
         }else{
             $type_condition = "A.type_id = '$typeID' AND";
         }
-        $sql = "SELECT A.aid AS ID, U.school_id,T.type_id, A.content, A.comment_num, T.type_name, A.hot, U.username, U.avatar, A.time, U.openID , A.comment_num, S.school_name, S.location_id, A.latitude,A.longitude,A.addresss
+        $sql = "SELECT A.aid AS ID, U.school_id,T.type_id, A.content, A.comment_num, T.type_name, A.hot, U.username, U.avatar, A.time, U.openID , A.comment_num, S.school_name, S.location_id, A.latitude,A.longitude,A.address
         FROM articles A, users U, article_types T ,schools S $tableCase
         WHERE $type_condition T.type_id = A.type_id 
         AND U.openID = A.openID 
