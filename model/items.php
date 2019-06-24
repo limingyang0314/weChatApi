999d4be6a8355d715b01239a542e769200578209
limingyang <limingyang0314@qq.com>
2019-06-24 23:04:55 +0800

    fix bug

--- model/items.php
+++ model/items.php
@@ -3,14 +3,27 @@ require_once 'mysql.php';
 
 function select_item_by_id($id, $conn){
     //echo $id;
-    $sql = "SELECT I.iID AS ID, U.username,U.openID, U.avatar, I.item_info AS content, T.type_name, I.hot, I.time ,I.comment_num, I.expect_price, I.contact_way, S.school_name, S.location_id
-    FROM items I, item_types T, users U, schools S, I.latitude,
-       I.longitude
+    $sql = "SELECT I.iID AS ID, 
+    U.username,
+    U.openID, 
+    U.avatar, 
+    I.item_info AS content, 
+    T.type_name, 
+    I.hot, I.time ,
+    I.comment_num, 
+    I.expect_price, 
+    I.contact_way, 
+    S.school_name, 
+    S.location_id, 
+    I.latitude,
+    I.longitude
+    FROM items I, item_types T, users U, schools S
     WHERE I.iID = $id 
     AND T.type_id = I.iType_ID 
     AND  S.sID = U.school_id
     AND U.openID = I.openID";
-    
+    echo $sql;
+    exit;
     //echo $sql;
     $result = mysqli_query($conn,$sql);
     $result = getDataAsArray($result);
@@ -72,8 +85,8 @@ function select_item_by_type($type, $limit, $page, $conn){
     ORDER BY I.time DESC 
     LIMIT {$start},{$limit}";
 
-    //echo $sql;
-    //exit;
+    echo $sql;
+    exit;
 
     $result = mysqli_query($conn,$sql);
     $result = getDataAsArray($result);
